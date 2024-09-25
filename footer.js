d05a5d8450722b5263eef3f0929403699f688ee0
Maximo <91247@sistemas.frc.utn.edu.ar>
2024-09-25 19:09:47 -0300

    Cambio de ubicacion y footer

--- footer.js
+++ footer.js
@@ -1,18 +1,36 @@
-//footer
 let mainFooter = document.querySelector(".nav-footer"),
-contactFooter = document.querySelector(".contact-footer");
+    contactFooter = document.querySelector(".contact-footer"),
+    emailContact = document.querySelector(".contact-footer p:first-child"),
+    phoneContact = document.querySelector(".contact-footer p:nth-child(2)"),
+    addressContact = document.querySelector(".contact-footer p:last-child"),
+    emailIcon = document.querySelector(".iconos-contacto i:first-child"),
+    locationIcon = document.querySelector(".iconos-contacto i:last-child");
 
 function eliminarElementosEnResponsive(mediaQuery) {
     if (mediaQuery.matches) {
-        // Agregar la clase "none" si el ancho de la ventana es igual o menor a 768px
+        // Ocultar el menú "Main" si el ancho de la ventana es igual o menor a 768px
         mainFooter.classList.add("none");
-        contactFooter.classList.add("none");
+
+        // Ocultar el email, dirección y sus respectivos íconos
+        emailContact.classList.add("none");
+        addressContact.classList.add("none");
+        emailIcon.classList.add("none");
+        locationIcon.classList.add("none");
+
+        // Mostrar solo el número de teléfono y su ícono
+        phoneContact.classList.remove("none");
     } else {
-        // Quitar la clase "none" si el ancho de la ventana es mayor a 768px
+        // Mostrar el menú "Main" si el ancho de la ventana es mayor a 768px
         mainFooter.classList.remove("none");
-        contactFooter.classList.remove("none");
+
+        // Volver a mostrar el email, dirección y sus respectivos íconos
+        emailContact.classList.remove("none");
+        addressContact.classList.remove("none");
+        emailIcon.classList.remove("none");
+        locationIcon.classList.remove("none");
     }
 }
+
 // Crear una instancia de MediaQueryList con la consulta para 768px o menos
 let mediaQuery = window.matchMedia("(max-width: 768px)");
 
